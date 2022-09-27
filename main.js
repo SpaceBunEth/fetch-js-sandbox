9349f18284723c006ac977c4b3f86dcd7a58474b
SpaceBun <jude.martinez@student.awesomeinc.org>
2022-09-27 17:33:29 +0000

    added axios to gitpod made a axios fetch function

--- main.js
+++ main.js
@@ -1,5 +1,35 @@
 console.log('hello world');
 
-fetch('https://api.coingecko.com/api/v3/ping')
- .then((responce) => responce.json())
- .then((data) => console.log(data))
+// fetch .then in a function
+function pingGecko() {
+    fetch('https://api.coingecko.com/api/v3/ping')
+    .then((responce) => responce.json())
+    .then((data) => console.log("fetch ", data))
+}
+
+pingGecko()
+
+
+// Aysnc Func
+let urlGecko = 'https://api.coingecko.com/api/v3/ping'
+let globalObj = {}
+
+async function pingGeckoAysnc(url) {
+    let responce = await fetch(url);
+    let data = await responce.json();
+    console.log("Async Func ", data)
+    
+}
+
+pingGeckoAysnc(urlGecko)
+
+// axios js npm package 
+async function getUser() {
+    try {
+      const response = await axios.get('https://api.coingecko.com/api/v3/ping');
+      console.log(response.data.gecko_says);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+getUser()
